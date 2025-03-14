10061992910eafd3975a57f565ffd6aaa3781134
unknown <muhamedmarshad18@gmail.com>
2025-03-14 22:59:17 +0530

    Add new depedencies

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE_BASE_PATH || "MainProject",
+  base:process.env.VITE_BASE_PATH ,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
